207f008f0204321eef2052a3d2f1d2ce5a6bc9f7
ntdat104 <ntd.k108cva@gmail.com>
2022-06-09 09:47:23 +0700

    add preview getPreviewVideos api

---
+++ src/controllers/preview.controller.ts
@@ -0,0 +1,19 @@
+import { PreviewVideosQuery } from '@/interfaces/preview/preview-videos-query';
+import { PreviewService } from '@/services/preview.service';
+import { Controller, Get, Query } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { Observable } from 'rxjs';
+
+@Controller('preview')
+export class PreviewController {
+  constructor(private readonly previewService: PreviewService) {}
+
+  @Get('preview-videos')
+  getPreviewVideos(
+    @Query() previewVideosQuery: PreviewVideosQuery,
+  ): Observable<AxiosResponse<any>> {
+    console.log(previewVideosQuery);
+    const { page = 0 } = previewVideosQuery;
+    return this.previewService.getPreviewVideos(page);
+  }
+}
